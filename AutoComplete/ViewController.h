0f766930f746159ed9c6122c9797469562361493
Katlyn Schwaebe <schwaebek@gmail.com>
2014-12-28 09:17:47 -0800

    Initial Commit

---
+++ AutoComplete/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoComplete
+//
+//  Created by Katlyn Schwaebe on 12/28/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoComplete/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoComplete
+//
+//  Created by Katlyn Schwaebe on 12/28/14.
+//  Copyright (c) 2014 Katlyn Schwaebe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
